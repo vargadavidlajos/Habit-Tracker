4512d127586fcc7cd1466f8f0b899d8590da5c45
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-30 16:38:42 +0100

    initial commit

---
+++ src/test/java/com/example/habit_tracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
